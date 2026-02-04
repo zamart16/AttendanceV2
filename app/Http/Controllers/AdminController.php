9138f4f2738c2733a6bc3bca89b77074d1038efe
raymartvapor143 <109051161+raymartvapor143@users.noreply.github.com>
2026-02-04 19:30:40 +0800

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,10 @@ use App\Models\Controller;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
 
+
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 class AdminController extends Controller
 {
     public function dashboard()
@@ -36,6 +40,21 @@ public function toggleController(Request $request, Controller $controller)
         'success' => true,
         'controller_id' => $newControllerId,
         'status' => $request->status
+    ]);
+}
+
+    // FEB 4 2026
+public function attendeesRealtime()
+{
+    $today = Carbon::now()->format('Y-m-d');
+
+    $attendees = DB::table('attendees')
+        ->where('attendance_date', $today)
+        ->orderBy('attendance_time', 'desc')
+        ->get();
+
+    return response()->json([
+        'attendees' => $attendees
     ]);
 }
 
